2917181f57a2b8bd9f7761c95be82b1c21877604
Adam Goforth <code@adamgoforth.com>
2018-02-17 23:24:35 +0100

    Fix some coding style issues
    Add pylint to the dev dependencies and use it to find coding style
    violations. This commit does not fix all issues, but most of them are
    now elminated.

--- createbillboardplaylist.py
+++ createbillboardplaylist.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python
 
-# This is the Create Billboard Charts YouTube Playlist script
-# It is a Python script that will download some of the current Billboard charts
-# and create YouTube playlists containing videos for all the songs for the charts.
-# If it is run regularly, it will create new playlists each week for the new Billboard
-# charts.
-#
-# An example of what the script creates can be seen here:
-# http://www.youtube.com/user/GimmeThatHotPopMusic
-#
+"""
+This is the Create Billboard Charts YouTube Playlist script
+It is a Python script that will download some of the current Billboard charts
+and create YouTube playlists containing videos for all the songs for the charts.
+If it is run regularly, it will create new playlists each week for the new Billboard
+charts.
+
+An example of what the script creates can be seen here:
+http://www.youtube.com/user/GimmeThatHotPopMusic
+"""
+
 # Copyright 2011-2018 Adam Goforth
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
-import httplib2
 import os.path
-import sys
 import time
 from ConfigParser import SafeConfigParser
 from datetime import datetime
 
+import httplib2
+
 # Google Data API
-import oauth2client
 from apiclient.discovery import build
+import oauth2client
 from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.tools import run_flow
@@ -42,8 +44,8 @@ from oauth2client.tools import run_flow
 # billboard.py
 import billboard
 
-# Almost every function needs the YouTube resource, so just use a global
-global youtube
+# Almost every function needs the YouTube resource, so define it globally
+youtube = None
 
 
 def get_video_id_for_search(query):
@@ -60,7 +62,7 @@ def get_video_id_for_search(query):
     ).execute()
 
     items = search_response.get('items', [])
-    if (len(items) == 0):
+    if not items:
         return None
 
     for item in items:
@@ -74,10 +76,14 @@ def get_video_id_for_search(query):
     return None
 
 def playlist_url_from_id(pl_id):
+    """Returns the URL of a playlist, given its ID"""
     return "https://www.youtube.com/playlist?list={0}".format(pl_id)
 
 def add_video_to_playlist(pl_id, video_id):
-    print ("\tAdding video pl_id: " + pl_id + " video_id: " + video_id)
+    """Adds the given video as the last video as the last one in the given
+    playlist
+    """
+    print "\tAdding video pl_id: " + pl_id + " video_id: " + video_id
 
     video_insert_response = youtube.playlistItems().insert(
         part="snippet",
@@ -95,13 +101,14 @@ def add_video_to_playlist(pl_id, video_id):
 
     title = video_insert_response['snippet']['title']
 
-    print('\tVideo added: {0}'.format(title.encode('utf-8')))
+    print '\tVideo added: {0}'.format(title.encode('utf-8'))
 
 def add_first_found_video_to_playlist(pl_id, search_query):
+    """Does a search for videos and adds the first result to the given playlist"""
     video_id = get_video_id_for_search(search_query)
 
     # No search results were found, so print a message and return
-    if video_id == None:
+    if video_id is None:
         print("No search results found for '" + search_query + "'. "
               "Moving on to the next song.")
         return
@@ -109,6 +116,7 @@ def add_first_found_video_to_playlist(pl_id, search_query):
     add_video_to_playlist(pl_id, video_id)
 
 def create_new_playlist(title, description):
+    """Creates a new, empty YouTube playlist with the given title and description"""
     playlists_insert_response = youtube.playlists().insert(
         part="snippet,status",
         body=dict(
@@ -133,6 +141,7 @@ def create_new_playlist(title, description):
     return pl_id
 
 def playlist_exists_with_title(title):
+    """Returns true if there is already a playlist in the channel with the given name"""
     playlists = youtube.playlists().list(
         part="snippet",
         mine=True,
@@ -147,22 +156,26 @@ def playlist_exists_with_title(title):
     return False
 
 def add_chart_entries_to_playlist(pl_id, entries):
+    """Given the list of entries from a billboard.py listing, search for a video for each
+    entry and add it to the given playlist
+    """
     song_count = 0
     for entry in entries:
         song_count += 1
-        if (song_count > 100):
+        if song_count > 100:
             break
 
         query = entry.artist + ' ' + entry.title
         song_info = ('#' + str(entry.rank) + ': ' + entry.artist + ' - ' +
-            entry.title)
+                     entry.title)
 
         print 'Adding ' + song_info
         add_first_found_video_to_playlist(pl_id, query)
 
-    print("\n---\n")
+    print "\n---\n"
 
 def create_playlist_from_chart(chart_id, chart_name, num_songs_phrase, web_url):
+    """Create and populate a new playlist with the current Billboard chart with the given ID"""
     # Get the songs from the Billboard web page
     chart = billboard.ChartData(chart_id)
     chart_date = datetime.strptime(chart.date, '%Y-%m-%d').strftime("%B %d, %Y")
@@ -178,40 +191,43 @@ def create_playlist_from_chart(chart_id, chart_name, num_songs_phrase, web_url):
     if playlist_exists_with_title(pl_title):
         print("Playlist already exists with title '" + pl_title + "'. "
               "Delete it manually and re-run the script to recreate it.")
-        return False
-    else:
-        pl_id = create_new_playlist(pl_title, pl_description)
-        add_chart_entries_to_playlist(pl_id, chart.entries)
-        return True
+        return
+
+    pl_id = create_new_playlist(pl_title, pl_description)
+    add_chart_entries_to_playlist(pl_id, chart.entries)
+    return
 
 def load_config_values():
-    # Load Config From settings.cfg
-    configPath = get_script_dir() + 'settings.cfg'
-    sectionName = 'accounts'
+    """Loads config values from the settings.cfg file in the script dir"""
+    config_path = get_script_dir() + 'settings.cfg'
+    section_name = 'accounts'
 
-    if (not os.path.exists(configPath)):
-        print "Error: No config file found. Copy settings-example.cfg to settings.cfg and customize it."
+    if not os.path.exists(config_path):
+        print ("Error: No config file found. Copy settings-example.cfg to "
+               "settings.cfg and customize it.")
         exit()
 
     config = SafeConfigParser()
-    config.read(configPath)
+    config.read(config_path)
 
     # Do basic checks on the config file
-    if not config.has_section(sectionName):
-        print "Error: The config file doesn't have an accounts section. Check the config file format."
+    if not config.has_section(section_name):
+        print ("Error: The config file doesn't have an accounts section. "
+               "Check the config file format.")
         exit()
 
-    if not config.has_option(sectionName, 'api_key'):
+    if not config.has_option(section_name, 'api_key'):
         print "Error: No developer key found in the config file.  Check the config file values."
         exit()
 
     config_values = {
-        'api_key': config.get(sectionName, 'api_key')
+        'api_key': config.get(section_name, 'api_key')
     }
 
     return config_values
-    
+
 def create_youtube_service(config):
+    """Create an instance of the YouTube service from the Google Data API library"""
     global youtube
 
     YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
@@ -234,8 +250,8 @@ def create_youtube_service(config):
 
     if credentials is None or credentials.invalid:
         parser = argparse.ArgumentParser(description=__doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            parents=[oauth2client.tools.argparser])
+                                         formatter_class=argparse.RawDescriptionHelpFormatter,
+                                         parents=[oauth2client.tools.argparser])
         flags = parser.parse_args()
 
         credentials = run_flow(flow, storage, flags)
@@ -249,24 +265,26 @@ def create_youtube_service(config):
     )
 
 def get_script_dir():
+    """Returns the absolute path to the script directory"""
     return os.path.dirname(os.path.realpath(__file__)) + '/'
 
 def main():
+    """Main script function"""
     print "### Script started at " + time.strftime("%c") + " ###\n"
 
     config = load_config_values()
     create_youtube_service(config)
 
     # Billboard Rock Songs
-    created = create_playlist_from_chart(
+    create_playlist_from_chart(
         "rock-songs",
         "Rock",
         "top 25 ",
         "http://www.billboard.com/charts/rock-songs"
     )
 
-    # Billboard R&B/Hip-Hop Songs 
-    created = create_playlist_from_chart(
+    # Billboard R&B/Hip-Hop Songs
+    create_playlist_from_chart(
         "r-b-hip-hop-songs",
         "R&B/Hip-Hop",
         "top 25 ",
@@ -274,7 +292,7 @@ def main():
     )
 
     # Billboard Dance/Club Play Songs
-    created = create_playlist_from_chart(
+    create_playlist_from_chart(
         "dance-club-play-songs",
         "Dance/Club Play",
         "top 25 ",
@@ -282,7 +300,7 @@ def main():
     )
 
     # Billboard Pop Songs
-    created = create_playlist_from_chart(
+    create_playlist_from_chart(
         "pop-songs",
         "Pop",
         "top 20 ",
@@ -290,7 +308,7 @@ def main():
     )
 
     # Billboard Hot 100
-    created = create_playlist_from_chart(
+    create_playlist_from_chart(
         "hot-100",
         "Hot 100",
         "",
